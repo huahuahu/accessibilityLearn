15039c7734a574a0b30af599798921a3fd73dbf2
Tiger Guo <teguo@microsoft.com>
2022-04-05 14:14:44 +0800

    Add cssdemo

--- Demo/Demo/Web/Model/WebItems.swift
+++ Demo/Demo/Web/Model/WebItems.swift
@@ -10,12 +10,16 @@ import Foundation
 enum WebItems: String, CaseIterable {
   case basic
   case divAsButton
+  case imageAlt
+  case tabbedInfo
 
   var item: Item {
     switch self {
     case .basic:
       return .detail(title: rawValue, vc: { WebBasicVC(htmlString: htmlString) })
     case .divAsButton:
+      return .detail(title: rawValue, vc: { WebBasicVC(htmlString: htmlString) })
+    case .imageAlt, .tabbedInfo:
       return .detail(title: rawValue, vc: { WebBasicVC(htmlString: htmlString) })
     }
   }
@@ -26,6 +30,10 @@ enum WebItems: String, CaseIterable {
       return htmlFrom(file: "Basic")
     case .divAsButton:
       return htmlFrom(file: "divButton")
+    case .imageAlt:
+      return htmlFrom(file: "imageAlt")
+    case .tabbedInfo:
+      return htmlFrom(file: "tabbedInfo")
     }
   }
 
